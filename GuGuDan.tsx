7cc45ab933a8c74f9ae4f54a738621edf765730c
zkunsang <zkunsang@gmail.com>
2021-07-17 18:53:11 +0900

    test

---
+++ GuGuDan.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { useState, useRef } from 'react';
+
+
+const GuGuDan = () => {
+    const [first, setFirst] = useState(Math.random() * 9);
+    const [second, setSecond] = useState(Math.random() * 9);
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+
+    const inputEl = useRef<HTMLInputElement>(null);
+
+    const onSubmitForm = (e: React.FormEvent) => {
+        e.preventDefault();
+        const input = inputEl.current;
+
+        if (parseInt(value) === first * second) {
+            setResult('correct');
+            setFirst(Math.random() * 9);
+            setSecond(Math.random() * 9);
+        } else {
+            setResult('incorrect');
+        }
+        setValue('');
+        if (input) {
+            input.focus();
+        }
+
+    }
+    return (
+        <>
+            <div>{first} x {second}?</div>
+            <form onSubmit={onSubmitForm}>
+                <input
+                    ref={inputEl}
+                    type="number"
+                    value={value}
+                    onChange={e => setValue(e.target.value)}></input>
+            </form>
+        </>
+    )
+}
+
+export default GuGuDan;
